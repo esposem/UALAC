84c64bb9a8adf893998bb411f243376c9248e7dd
Emanuele <e.emanuelegiuseppe@yahoo.it>
2016-12-09 10:47:31 +0100

    web socket not really finished

--- Atelier/socket-server.js
+++ Atelier/socket-server.js
@@ -8,9 +8,14 @@ module.exports = function(httpServer) {
 	// Socket.io server
 	io.on('connect', function(socket){
 		// console.log('Connected')
+		// socket.emit('onopen');
 
 		socket.on('disconnect', function(){
 			// console.log('Disconnected')
+		})
+
+		socket.on('mess', function(){
+			console.log("GOT A MESS");
 		})
 
 		socket.on('error', function(err){
