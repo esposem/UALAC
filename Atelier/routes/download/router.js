2484a819d9ac5ff55c319188e5cecab90918056f
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2016-12-12 16:27:53 +0100

    included little file to explain

--- Atelier/routes/download/router.js
+++ Atelier/routes/download/router.js
@@ -157,6 +157,10 @@ router.post('/', function(req, res, next) {
     `<the-temp  \n text=\"false\" >  \n`  + req.body.text.ob  + `\n</the-temp>` +
     "</body> \n " +
   "</html> \n ");
+
+  appf.file("how_to_run.txt", " With terminal, go in /app and then write `bower install`. After all the files are created \n" +
+  "run polyserve. The result will be in localhost:8080.");
+  
   zip
   .generateNodeStream({type:'nodebuffer',streamFiles:true})
   .pipe(fs.createWriteStream('./out.zip'))
